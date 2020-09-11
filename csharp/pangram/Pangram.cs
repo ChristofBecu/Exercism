c48ed481f90bb943337d484c2af87a9cb6418411
ChristofBecu <christof.becu@gmail.com>
2020-09-11 22:58:09 +0200

    Add old pangram solution

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        var alphabet = new HashSet<char>("abcdefghijklmnopqrstuvwxyz");
+        var cleanInput = new HashSet<char>(input.ToLower());
+        cleanInput.RemoveWhere(character => !char.IsLetter(character));
+        return cleanInput.SetEquals(alphabet);
+    }
+}
