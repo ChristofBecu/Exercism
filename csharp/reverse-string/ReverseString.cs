8e0f43e807f900a69a72aeca2829f78b3f85207e
ChristofBecu <christof.becu@gmail.com>
2020-09-11 22:28:05 +0200

    Add old solution reverse string

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        // 
+        char[] inputArray = input.ToCharArray();
+        Array.Reverse( inputArray );
+        return new string( inputArray );
+
+        // Classic reverse of string
+        /*
+        string reversedString = ""; 
+        for ( int i = input.Length-1; i >= 0; i-- ) 
+            reversedString += input[i];
+        return reversedString; 
+        */
+
+        // Recusive ternary operator
+        /*
+        return ( input == "") ? "" : Reverse(input.Substring(1)) + input[0];
+        */
+        
+    }
+}
