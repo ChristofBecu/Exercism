497d317166dd239b847e2632dccde058895ff649
ChristofBecu <christof.becu@gmail.com>
2020-09-12 09:50:30 +0200

    Add old sum of multiples solution

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        int sum = 0;
+        HashSet<int> unique = ExtractUniqueMultiples(multiples, max);
+
+        foreach (int i in unique)
+            sum += i;
+
+        return sum;
+    }
+
+    private static HashSet<int> ExtractUniqueMultiples(IEnumerable<int> multiples, int max)
+    {
+        HashSet<int> unique = new HashSet<int>();
+        foreach (int i in multiples)
+        {
+            if (i != 0)
+            {
+                for (int j = i; j < max; j++)
+                {
+                    if (j % i == 0)
+                        unique.Add(j);
+                }
+            }
+        }
+
+        return unique;
+    }
+}
