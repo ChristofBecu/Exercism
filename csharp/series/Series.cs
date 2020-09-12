6b61904c84321bd43830bc72dd5bea5088f9d8c9
ChristofBecu <christof.becu@gmail.com>
2020-09-12 11:33:49 +0200

    Refactor Series solution

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,17 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        if (sliceLength <= 0 || sliceLength > numbers.Length || numbers == "")
-            throw new ArgumentException();
-
-        List<string> sliceList = new List<string>();
-        for (int i = 0; i <= numbers.Length - sliceLength; i++) 
-            sliceList.Add( numbers.Substring(i, sliceLength));
-        
-        return sliceList.ToArray(); 
+        if (sliceLength > 0 && sliceLength <= numbers.Length)
+            return Enumerable.Range(0, numbers.Length - sliceLength + 1)
+                .Select(x => numbers.Substring(x, sliceLength)).ToArray();
+        throw new ArgumentException();
     }
 }
