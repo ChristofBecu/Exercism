6e734537502ac0f0d1a6abc8a669a6ebb8aacd5e
ChristofBecu <christof.becu@gmail.com>
2020-09-12 11:34:35 +0200

    Add old Grains solution

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n <= 0 || n > 64)
+            throw new ArgumentOutOfRangeException();
+        return n == 1 ? 1 : 1UL << (n - 1);
+    }
+    public static ulong Total() => 2 * ((1UL << 63) - 1 ) + 1;
+}
