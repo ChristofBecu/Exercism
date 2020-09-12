f649920df72a82ebc407209964b26823cfd4c2c8
ChristofBecu <christof.becu@gmail.com>
2020-09-12 16:23:01 +0200

    Add old Isogram solution

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) 
+    {
+        word =  RemoveNonAlphabetics(word.ToLower());
+        return word == RemoveDuplicateCharacters(word);
+    }
+
+    public static string RemoveDuplicateCharacters(string s) => string.Join("", new HashSet<char>(s).ToArray());
+
+    public static string RemoveNonAlphabetics(string s) => Regex.Replace(s,"[^A-Za-z]",""); 
+
+}
