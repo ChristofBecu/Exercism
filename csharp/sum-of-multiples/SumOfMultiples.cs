de9085679cfce2d7b498bef11d119d4194c38850
ChristofBecu <christof.becu@gmail.com>
2020-09-13 12:17:34 +0200

    Refactor SumOfMultiples to handle big integers

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -4,10 +4,21 @@ using System.Linq;
 
 public static class SumOfMultiples
 {
-    public static int Sum(IEnumerable<int> multiples, int max) => 
-        (   from dividend in Enumerable.Range(1, max - 1)
-            from divisor in multiples
-            where divisor != 0 && dividend % divisor == 0
-            select dividend
-        ).Distinct().Sum();
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        if (multiples.Any() && max > multiples.Min())
+        {
+            List<int> multiplesRange = new List<int>();
+            for (int i = multiples.Min(); i < max; i++)
+                multiplesRange.Add(i);
+
+            return (from dividend in multiplesRange
+                    from divisor in multiples
+                    where divisor != 0 && dividend % divisor == 0
+                    select dividend)
+                .Distinct()
+                .Sum();
+        }
+        return 0;
+    }
 }
