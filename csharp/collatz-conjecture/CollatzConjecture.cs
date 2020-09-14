ef70f5c68a672a9ff328385f1c0918f1d6ece703
ChristofBecu <christof.becu@gmail.com>
2020-09-14 12:36:49 +0200

    Refactor CollatzConjecture solution
    recursive & ternary condition

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -2,18 +2,12 @@
 
 public static class CollatzConjecture
 {
-    public static int Steps(int number)
-    {
-        int steps = 0;
-        if ( number <= 0 )
-            throw new ArgumentException();
-        while ( number > 1 ) {
-            steps++;
-            if ( (number & 1) == 0)
-                number /= 2;
-            else
-                number = number * 3 + 1;
-        }
-        return steps;
-    }
+    public static int Steps(int number) =>
+        number < 1
+            ? throw new ArgumentOutOfRangeException()
+            : number == 1
+                ? 0
+                : number % 2 == 0
+                    ? Steps(number /= 2) + 1
+                    : Steps(number * 3 + 1) + 1;
 }
