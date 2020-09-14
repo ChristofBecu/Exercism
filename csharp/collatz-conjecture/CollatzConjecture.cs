07212ec6936b4bc44aebbb50e74000d1177a562b
ChristofBecu <christof.becu@gmail.com>
2020-09-14 11:53:35 +0200

    Add old CollatzConjecture solution

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        int steps = 0;
+        if ( number <= 0 )
+            throw new ArgumentException();
+        while ( number > 1 ) {
+            steps++;
+            if ( (number & 1) == 0)
+                number /= 2;
+            else
+                number = number * 3 + 1;
+        }
+        return steps;
+    }
+}
