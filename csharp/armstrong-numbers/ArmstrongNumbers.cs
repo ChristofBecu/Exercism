04f4b284f640367c13c456e170b570c05fdf50cd
ChristofBecu <christof.becu@gmail.com>
2020-09-18 13:27:22 +0200

    Add ArmstrongNumbers solution

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        var bases = number.ToString().ToCharArray().Select(c => (byte)c - 48);
+        var exponent = bases.Count();
+        double armstrong = 0.0;
+
+        foreach (int b in bases)
+            armstrong += Math.Pow(b, exponent);
+
+        return armstrong == number;
+    }
+}
